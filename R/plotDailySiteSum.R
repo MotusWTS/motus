b085b91339b7650d5eb5999323f07733a0502617
Crysler <Zoe.Crysler@ec.gc.ca>
2018-01-24 12:27:39 -0800

    name updates

--- R/plotDailySiteSum.R
+++ R/plotDailySiteSum.R
@@ -29,10 +29,10 @@ plotDailySiteSum <- function(data, recvDeployName){
   }
   sitesum <- siteSumDaily(filter(data, recvDeployName == recvDeployName))
   detections <- ggplot2::ggplot(sitesum, ggplot2::aes(date, num_det)) +
-    ggplot2::geom_bar(stat = "identity") + ggplot2::theme_bw() + ## creates bar plot by recvDeployname
+    ggplot2::geom_bar(stat = "identity") + ggplot2::theme_bw() + ## creates bar plot by recvDeployName
     ggplot2::labs(x= "Date", y = "Total detections")
   tags <- ggplot2::ggplot(sitesum, ggplot2::aes(date, num_tags)) +
-    ggplot2::geom_bar(stat = "identity") + ggplot2::theme_bw() + ## creates line graph by recvDeployname
+    ggplot2::geom_bar(stat = "identity") + ggplot2::theme_bw() + ## creates line graph by recvDeployName
     ggplot2::labs(x= "Date", y = "Number of tags")
-  gridExtra::grid.arrange(detections, tags, nrow = 2, top = paste("Daily number of detections and tags at", recvDeployname, sep = " "))
+  gridExtra::grid.arrange(detections, tags, nrow = 2, top = paste("Daily number of detections and tags at", recvDeployName, sep = " "))
 }
