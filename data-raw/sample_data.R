5cd1fb995bd9a864abfcb924b6a95efd7b554942
Steffi LaZerte <steffi@steffi.ca>
2019-02-08 12:38:26 -0600

    Create sample data for examples and testing

---
+++ data-raw/sample_data.R
@@ -0,0 +1,9 @@
+# Create a small subset data frame for testing and examples
+
+tags <- tagme(projRecv = 176, new = FALSE, update = TRUE, "./data-raw")
+shorebirds <- dplyr::tbl(tags, "alltags") %>%
+  dplyr::collect()
+
+usethis::use_data(shorebirds, overwrite = TRUE)
+
+#file.remove("./data-raw/project-176.motus") # Keep this file? 9 megs... not too bad
