d544da2d930935f7bbc1692f2696db8cfd35e704
Steffi LaZerte <steffi@steffi.ca>
2019-02-08 17:23:53 -0600

    Fix bugs and add functionality - Be explicit about ggmap version requirements - Use stamen maps instead of google maps to avoid googlemaps key requirement - Provide option of automatically getting lat/lon and date ranges from the data

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -2,10 +2,11 @@ context("Plotting functions")
 
 
 test_that("Plots run with no errors", {
+  shorebirds_sql <- motus::tagme(176, update = FALSE, dir = system.file("extdata", package = "motus"))
   expect_silent(plotAllTagsCoord(shorebirds))
   expect_silent(plotAllTagsSite(shorebirds))
   expect_silent(plotDailySiteSum(shorebirds, "Longridge"))
-  expect_silent(plotRouteMap(shorebirds))
+  expect_silent(plotRouteMap(shorebirds_sql, recvStart = "2016-01-01", recvEnd = "2016-12-31"))
   expect_silent(plotSite(shorebirds))
   expect_silent(plotSiteSig(shorebirds, "Longridge"))
   expect_silent(plotTagSig(shorebirds, "16035"))
