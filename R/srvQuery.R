9e28040f06eb0b0f51ec9db63a49f46d17135f14
Steffi LaZerte <steffi@steffi.ca>
2019-02-13 15:11:52 -0600

    Switch from RCurl to httr - Remove option for "get", only use "post" - use message rather than cat so can be suppressed - format documentation for srvQuery and don't list in index

--- R/srvQuery.R
+++ R/srvQuery.R
@@ -1,18 +1,15 @@
 #' Send a query to the data server API
 #'
 #' @param API one of the Motus$API_DATA_... constants
-#'
 #' @param params named list of API-specific parameters
-#'
-#' @param requestType "post" or "get"; default: "post".
-#'
-#' @param show if TRUE, print the request to the console before submitting to motus
-#'
-#' @param JSON if TRUE, return results as JSON-format string; otherwise, as R list
-#'
-#' @param auth if TRUE, the default, the API requires an
-#'     authentication token, which will be included automatically from \code{Motus$authToken}
-#' Otherwise, an authentication token will be requested using the credentials in \code{Motus[c("userLogin", "userPassword")]}
+#' @param show if TRUE, print the request to the console before submitting to
+#'   motus
+#' @param JSON if TRUE, return results as JSON-format string; otherwise, as R
+#'   list
+#' @param auth if TRUE, the default, the API requires an authentication token,
+#'   which will be included automatically from \code{Motus$authToken} Otherwise,
+#'   an authentication token will be requested using the credentials in
+#'   \code{Motus[c("userLogin", "userPassword")]}
 #'
 #' @return the result of sending the request to the data server API.  The
 #'     result is a JSON-format character scalar if \code{json} is
@@ -24,18 +21,18 @@
 #'
 #' @author John Brzustowski
 #'     \email{jbrzusto@@REMOVE_THIS_PART_fastmail.fm}
+#'     
+#' @keywords internal
 
-srvQuery = function (API, params = NULL, requestType="post", show=FALSE, JSON=FALSE, auth=TRUE) {
-    curl = RCurl::getCurlHandle()
-    RCurl::curlSetOpt(curl=curl,
-                      .opts = list(
-                          httpheader = c(
-                              "Accept"="application/json"),
-                          timeout = 300,
-                          verbose = FALSE)
-                      )
+srvQuery <- function (API, params = NULL, show = FALSE, JSON = FALSE, auth = TRUE) {
 
-    URL = file.path(Motus$dataServerURL, API)
+    url <- file.path(Motus$dataServerURL, API)
+    ua <- httr::user_agent(agent = "http://github.com/MotusWTS/motus")
+    
+    # Set curl options
+    httr::set_config(httr::accept_json())
+    httr::set_config(httr::timeout(300))
+    #httr::set_config(httr::verbose())
 
     for (i in 1:2) {
         ## at most two iterations; the second allows for
@@ -55,23 +52,22 @@ srvQuery = function (API, params = NULL, requestType="post", show=FALSE, JSON=FA
         }
         query = c(query, params)
 
-        json = query %>% jsonlite::toJSON (auto_unbox=TRUE, null="null")
+        json = jsonlite::toJSON(query, auto_unbox = TRUE, null = "null")
 
-        if(show)
-            cat(json, "\n")
-
-        if (requestType == "post")
-            resp = RCurl::postForm(URL, json=json, .encoding="utf-8", style="post", curl=curl, .contentEncodeFun=RCurl::curlPercentEncode)
-        else
-            resp = RCurl::getForm(URL, json=json, .encoding="utf-8", curl=curl)
-        resp = memDecompress(resp, "bzip2", asChar=TRUE)
+        if(show) message(json, "\n")
+    
+        resp <- httr::POST(url, body = list("json" = json), encode = "form",
+                           httr::config(http_content_decoding = 0)) %>%
+            httr::content(as = "raw") %>%
+            memDecompress("bzip2", asChar = TRUE)
+        
 		Encoding(resp) = "UTF-8"
 
-        if (JSON)
-            return (resp)
-        if (grepl("^[ \r\n]*$", resp))
-            return(list())
-        rv = jsonlite::fromJSON(resp)
+        if (JSON) return(resp)
+        if (grepl("^[ \r\n]*$", resp)) return(list())
+		
+        rv <- jsonlite::fromJSON(resp)
+        
         if (! is.null(rv$error)) {
             if (rv$error %in% c("token expired", "token invalid")) {
                 Motus$authToken = NULL
