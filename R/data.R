f95db3f3b9bda52d4ea18eaaadefd3d411714813
Steffi LaZerte <steffi@steffi.ca>
2019-03-28 11:31:26 -0500

    Add documention for data

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Example shorebirds tags download from Motus
+#'
+#' An example data frame of shorebirds data (project 176)
+#'
+#' @format A data frame with 108826 rows and 60 variables:
+"shorebirds"
