bef498495ee1a8e9ebe9d1dc33e2f0782c7281cd
Steffi LaZerte <steffi@steffi.ca>
2019-06-20 12:52:16 -0500

    Update included data

--- data-raw/sample_data.R
+++ data-raw/sample_data.R
@@ -1,4 +1,7 @@
 # Create a small subset data frame for testing and examples
+sessionVariable(name = "userLogin", val = "motus.sample")
+sessionVariable(name = "userPassword", val = "motus.sample")
+
 file.remove("./inst/extdata/project-176.motus")
 tags <- tagme(projRecv = 176, new = TRUE, update = TRUE, "./inst/extdata/")
 shorebirds <- dplyr::tbl(tags, "alltags") %>%
