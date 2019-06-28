37c0513608b69bfafb8f15e4911738c413feaf77
Steffi LaZerte <steffi@steffi.ca>
2019-06-28 08:57:29 -0500

    Add release script

---
+++ RELEASE.R
@@ -0,0 +1,37 @@
+# Steps/Commands to run before a package release -----------------------------
+
+## Update internal data files
+source("data-raw/updatesql.R")
+source("data-raw/sample_data.R")
+
+## Documentation
+# Update NEWS
+
+# Check spelling
+dict <- hunspell::dictionary('en_CA')
+devtools::spell_check()
+
+## Finalize package version
+
+## Checks
+devtools::check(run_dont_test = TRUE)   # Local, run long-running examples
+
+## Windows checks (particularly if submitting to CRAN)
+devtools::check_win_release() # Win builder
+devtools::check_win_devel()
+devtools::check_win_oldrelease()
+
+## Run in console
+system("cd ..; R CMD build motus")
+system("cd ..; R CMD check motus_1.5.0.tar.gz --as-cran")
+
+## Push to github
+## Check travis / appveyor
+
+## Check Reverse Dependencies (are there any?)
+#tools::dependsOnPkgs("naturecounts")
+#devtools::revdep()
+
+## Actually release it, create signed release on github
+system("git tag -s v1.5.0 -m 'v1.5.0'")
+system("git push --tags")
