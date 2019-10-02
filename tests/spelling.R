9fa3f0af9ce3338d010b73033e8ef24ae1494fdd
Steffi LaZerte <steffi@steffi.ca>
2019-10-02 10:28:52 -0500

    Skip spelling on appveyor

--- tests/spelling.R
+++ tests/spelling.R
@@ -1,6 +1,5 @@
-if(requireNamespace('spelling', quietly = TRUE) &&
-   requireNamespace('hunspell', quietly = TRUE)) {
-  dictionary("en_CA")
+if(requireNamespace('spelling', quietly = TRUE)) {
+  testthat::skip_on_appveyor()
   spelling::spell_check_test(vignettes = TRUE, error = FALSE,
                              skip_on_cran = TRUE)
 }
