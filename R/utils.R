c779b353bdee00bbf7688b95690ffc7604a67091
Steffi LaZerte <steffi@steffi.ca>
2019-10-08 10:06:20 -0500

    Create helper functions

---
+++ R/utils.R
@@ -0,0 +1,64 @@
+#' Skip if not testing locally with authorization
+#' 
+#' All testthat tests that require a personal user account are prefaced with
+#' this skip function. 
+#' 
+#' The credentials for a personal user account (`motus_userLogin` and
+#' `motus_userPassword`) should be stored in the users .Renviron file (generally
+#' found in the users Home, e.g., on linux /home/user/ which is loaded on R
+#' startup. If the credentials are not found, the tests are skipped.
+#' 
+#' @noRd
+
+skip_if_no_auth <- function() {
+  if (identical(Sys.getenv("motus_userLogin"), "")) {
+    skip("No authentication available")
+  } else {
+    warning("Setting local auth")
+    sessionVariable(name = "userLogin", val = Sys.getenv("motus_userLogin"))
+    sessionVariable(name = "userPassword", val = Sys.getenv("motus_userPassword"))
+  }
+}
+
+#' Test for local authorization
+#' 
+#' This is a helper function for testing and applying local authorizations when
+#' available.
+#' 
+#' The credentials for a personal user account (`motus_userLogin` and
+#' `motus_userPassword`) should be stored in the users .Renviron file (generally
+#' found in the users Home, e.g., on linux /home/user/ which is loaded on R
+#' startup. If the credentials are not found, they are applied and TRUE is
+#' returned. Otherwise FALSE is returned.
+#' 
+#' @noRd
+
+have_auth <- function() {
+  if (identical(Sys.getenv("motus_userLogin"), "")) {
+    auth <- FALSE
+  } else {
+    sessionVariable(name = "userLogin", val = Sys.getenv("motus_userLogin"))
+    sessionVariable(name = "userPassword", val = Sys.getenv("motus_userPassword"))
+    auth <- TRUE
+  }
+  auth
+}
+
+# Is it a project or a receiver?
+is_proj <- function(x) stringr::str_detect(x, "^[0-9]+$")
+
+# Get project or receiver from source name
+get_projRecv <- function(src) {
+  if (! inherits(src, "src_sql"))
+    stop("src is not a dplyr::src_sql object")
+  
+  projRecv <- basename(src[[1]]@dbname)
+  if(stringr::str_detect(projRecv, "project-[0-9]+.motus")) {
+    projRecv <- as.numeric(stringr::str_extract(projRecv, "[0-9]+"))
+  } else if(stringr::str_detect(projRecv, ".motus")) {
+    projRecv <- stringr::str_remove(projRecv, ".motus")
+  } else {
+    stop("Database is not a recognized motus project", call. = FALSE)
+  }
+  projRecv
+}
