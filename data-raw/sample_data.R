8c189c7cf4d2caae9482eb0461276e6d92dd7d93
Steffi LaZerte <steffi@steffi.ca>
2019-10-09 09:20:42 -0500

    Update internal data

--- data-raw/sample_data.R
+++ data-raw/sample_data.R
@@ -1,6 +1,6 @@
 # Create a small subset data frame for testing and examples
-sessionVariable(name = "userLogin", val = "motus.sample")
-sessionVariable(name = "userPassword", val = "motus.sample")
+
+sample_auth()
 
 file.remove("./inst/extdata/project-176.motus")
 tags <- tagme(projRecv = 176, new = TRUE, update = TRUE, "./inst/extdata/")
@@ -10,3 +10,8 @@ shorebirds <- dplyr::tbl(tags, "alltags") %>%
 usethis::use_data(shorebirds, overwrite = TRUE)
 
 #file.remove("./data-raw/project-176.motus") # Keep this file?
+
+if(have_auth()) {
+  file.remove("./inst/extdata/SG-3115BBBK0782.motus")
+  tagme("SG-3115BBBK0782", new = TRUE, update = TRUE, dir = "./inst/extdata/")
+}
