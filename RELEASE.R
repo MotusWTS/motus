277ab264ae075e456925d3c0841b2254fa574e49
steffilazerte <steffi@steffi.ca>
2022-11-16 14:02:47 -0600

    Use new version in release

--- RELEASE.R
+++ RELEASE.R
@@ -72,7 +72,7 @@ spelling::update_wordlist() # All remaining words will be added to the ignore WO
 ## Finalize package version
 # - Update DESCRIPTION - package version
 # - Update .onLoad - API version
-v <- "6.0.0"
+v <- "6.0.1"
 v <- packageVersion("motus") # If dev version loaded with devtools::load_all()
 
 ## Checks
